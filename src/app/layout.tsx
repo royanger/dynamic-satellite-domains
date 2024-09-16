0277aa1811e51f99cd0ca0a9e98134ee98d8de47
Roy Anger <roy@royanger.com>
2024-09-16 13:39:32 -0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import './globals.css';
+import { headers } from 'next/headers';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  // get the current host/domain
+  const headersList = headers()
+  const host = headersList.get('x-forwarded-host')
+
+  // set the sign-in and sign-up URLs
+  // const primarySignInUrl = 'http://localhost:3000/sign-in'
+  // const primarySignUpUrl = 'https://localhost:3000/sign-up'
+
+  const satellites = ['http://royanger.work']
+
+  return (
+    <ClerkProvider
+      isSatellite={host !== process.env.NEXT_PUBLIC_ROOT_DOMAIN}
+      domain={host!}
+      allowedRedirectOrigins={satellites}
+    >
+      <html lang='en'>
+        <body>
+          <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
