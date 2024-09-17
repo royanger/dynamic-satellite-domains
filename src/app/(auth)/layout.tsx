f3ded9ab5cbce02ef71799b08f31d1b165384847
Roy Anger <roy@royanger.com>
2024-09-17 13:10:52 -0400

    testing

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
+
+
+// get the current host/domain
+const headersList = headers()
+const host = headersList.get('x-forwarded-host')
+
+
+if (host !== process.env.NEXT_PUBLIC_ROOT_DOMAIN) redirect(process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL!)
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return children
+}
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,4 @@
 import { SignIn } from "@clerk/nextjs";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-
-
-// get the current host/domain
-const headersList = headers()
-const host = headersList.get('x-forwarded-host')
-
-
-if (host !== process.env.NEXT_PUBLIC_ROOT_DOMAIN) redirect(process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL!)
 
 export default function SignInPage() {
   return <SignIn />
