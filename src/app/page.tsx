5e825cb1df29e3d6522cab0817283640d7e4794d
Roy Anger <roy@royanger.com>
2025-03-27 22:36:51 -0400

    added org switcher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
       <p>You are currently on the home page</p>
+      <OrganizationSwitcher />
     </div>
   );
 }
